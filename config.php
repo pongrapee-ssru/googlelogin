946cda3ef8609b8aab327edac7d41537b511e2d3
pongrapee-ssru <168944653+pongrapee-ssru@users.noreply.github.com>
2024-10-07 07:56:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-
 $client_id = 'YOUR_CLIENT_ID';
 $client_secret = 'YOUR_CLIENT_SECRET';
 $redirect_uri = 'YOUR_REDIRECT_URI';
+?>
+
+// Replace 'YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET', and 'YOUR_REDIRECT_URI' with credentials created/obtained from GCP
